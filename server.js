6e0aa9abe4d798a8fdb0c71cfc7e30f0858b2fc9
Surendar <shobana@Surendars-MacBook-Air.local>
2018-09-13 23:15:40 +0530

    push server

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var app = express()
 app.use(express.static(__dirname));
 
 app.get('/', function (req, res) {
-    res.sendFile(path.resolve(__dirname, 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'app/build/index.html'));
 })
 
 app.listen(process.env.PORT || 3000, function () {
