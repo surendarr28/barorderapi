22a5b0c945674f8625f809fe1c891967eab2f590
Surendar <shobana@Surendars-MacBook-Air.local>
2018-09-13 23:21:08 +0530

    change path

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ var express = require('express')
 var path = require('path');
 var app = express()
 
-app.use(express.static(__dirname));
+app.use(express.static(path.resolve(__dirname, 'app/build')));
+console.log(path.resolve(__dirname, 'app/build'));
 
 app.get('/', function (req, res) {
     res.sendFile(path.resolve(__dirname, 'app/build/index.html'));
