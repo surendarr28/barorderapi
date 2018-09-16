d490d06a324f6cbe0451ee600f597d4cd26efb12
Surendar <shobana@Surendars-MacBook-Air.local>
2018-09-16 15:00:52 +0530

    get table by order added

--- server.js
+++ server.js
@@ -10,13 +10,31 @@ const client = new Client({
 
 client.connect();
 
+app.use(express.static(path.resolve(__dirname, 'app/build')));
+console.log(path.resolve(__dirname, 'app/build'));
+
 app.get('/v1/api/tables', function (req, res) {
     try {
         client.query('SELECT * from tblTable where isAvail = true;', (err, result) => {
             if (err) return res.send("Some Error");
             return res.send(result.rows);
-            
+
         });
+    } catch (e) {
+        return res.send("Some Error");
+    }
+});
+
+app.get('/v1/api/tables/:tableId', function (req, res) {
+    try {
+        client.query('select item."name", oim.status, oim.quantity, oim.order_id, item.item_id from tblordertablemapping as otm' +
+            'join tblorederitemmapping as oim on oim.order_id = otm.order_id' +
+            'join tblitem as item on item.id = oim.item_id' +
+            'where otm.table_id = ' + req.params.tableId + ' and otm.orderstatus = 1', (err, result) => {
+                if (err) return res.send("Some Error");
+                return res.send(result.rows);
+
+            });
     } catch (e) {
         return res.send("Some Error");
     }
